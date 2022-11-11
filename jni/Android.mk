fa1b01b0882fe3d2617e8e5c1fc85b8b0dc4d687
iusmac <iusico.maxim@libero.it>
2022-11-11 14:41:50 +0100

    Import JNI library for QCOM SoC
    * from https://github.com/LineageOS/android_vendor_qcom_opensource_libfmjni/commits/lineage-18.1
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

---
+++ jni/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    FmIoctlsInterface.cpp \
+    ConfigFmThs.cpp \
+    FmPerformanceParams.cpp \
+    ConfFileParser.cpp \
+    FmRadioController.cpp \
+    LibfmJni.cpp
+
+LOCAL_C_INCLUDES := $(JNI_H_INCLUDE) \
+    frameworks/base/core/jni/include \
+    frameworks/base/include/media
+
+LOCAL_SHARED_LIBRARIES := \
+    libcutils \
+    libdl \
+    liblog \
+    libmedia \
+    libnativehelper \
+
+ifeq ($(TARGET_QCOM_NO_FM_FIRMWARE),true)
+    LOCAL_CFLAGS += -DQCOM_NO_FM_FIRMWARE
+endif
+
+LOCAL_MODULE := libfmjni
+include $(BUILD_SHARED_LIBRARY)
