970ebae4886733209a7cb393c98316def03e81c3
iusmac <iusico.maxim@libero.it>
2022-11-11 14:37:25 +0100

    Override FMRadio
    Some ROMs still include that old radio, even support is dropped since
    LOS 18.1.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- Android.mk
+++ Android.mk
@@ -29,6 +29,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := RevampedFMRadio
 
+LOCAL_OVERRIDES_PACKAGES := FMRadio
+
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
 LOCAL_JNI_SHARED_LIBRARIES := libfmjni
