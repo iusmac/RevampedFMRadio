dc4e1675e765f850287a19f2cbde10516aed61d5
Erfan Abdi <erfangplus@gmail.com>
2023-07-20 23:26:45 +0330

    FMRadio: Use FLAG_IMMUTABLE for notifications
    * in preparation for targetSdk 31
    
    Change-Id: I9d2d9c60655e3e165d3639436c5a18d8ce33710e

--- src/com/android/fmradio/FmService.java
+++ src/com/android/fmradio/FmService.java
@@ -1850,7 +1850,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
             aIntent.addCategory(Intent.CATEGORY_LAUNCHER);
             aIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             aIntent.setClassName(getPackageName(), mTargetClassName);
-            PendingIntent pAIntent = PendingIntent.getActivity(mContext, 0, aIntent, 0);
+            PendingIntent pAIntent = PendingIntent.getActivity(mContext, 0, aIntent, PendingIntent.FLAG_IMMUTABLE);
 
             if (mNotificationManager == null) {
                 mNotificationManager = (NotificationManager)
@@ -1895,17 +1895,17 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
 
             Intent intent = new Intent(FM_SEEK_PREVIOUS);
             intent.setClass(mContext, FmService.class);
-            PendingIntent pIntent = PendingIntent.getService(mContext, 0, intent, 0);
+            PendingIntent pIntent = PendingIntent.getService(mContext, 0, intent, PendingIntent.FLAG_IMMUTABLE);
             notificationBuilder.addAction(R.drawable.btn_fm_prevstation,
                     getString(R.string.accessibility_prev), pIntent);
             intent = new Intent(isPlaying ? FM_TURN_OFF : FM_TURN_ON);
             intent.setClass(mContext, FmService.class);
-            pIntent = PendingIntent.getService(mContext, 0, intent, 0);
+            pIntent = PendingIntent.getService(mContext, 0, intent, PendingIntent.FLAG_IMMUTABLE);
             notificationBuilder.addAction(playButtonResId,
                     getString(playButtonTitleResId), pIntent);
             intent = new Intent(FM_SEEK_NEXT);
             intent.setClass(mContext, FmService.class);
-            pIntent = PendingIntent.getService(mContext, 0, intent, 0);
+            pIntent = PendingIntent.getService(mContext, 0, intent, PendingIntent.FLAG_IMMUTABLE);
             notificationBuilder.addAction(R.drawable.btn_fm_nextstation,
                     getString(R.string.accessibility_next) , pIntent);
             notificationBuilder.setContentIntent(pAIntent);
