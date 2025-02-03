7b744d4481d875db841ed6c5f6c0078ebf116d22
iusmac <iusico.maxim@libero.it>
2025-02-03 23:49:27 +0100

    Fix speaker/headphone icon getting out of sync when headset connected
    A wrong speaker/headphone icon is shown when headset are connected and
    FM is powered down then powered up. To fix, we shall take into the
    account user selection.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- src/com/android/fmradio/FmService.java
+++ src/com/android/fmradio/FmService.java
@@ -2510,7 +2510,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
                 case FmListener.MSGID_POWERUP_FINISHED:
                     bundle = msg.getData();
                     handlePowerUp(bundle);
-                    mIsSpeakerUsed = !isHeadSetIn();
+                    mIsSpeakerUsed = !isHeadSetIn() || isSpeakerPhoneOn();
                     break;
 
                 // power down
