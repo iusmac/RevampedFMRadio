ae5a7fc77830773b08db61c9ea0f9fd57bd3cb4c
tavukkdoner <166294260+tavukkdoner@users.noreply.github.com>
2025-07-11 06:35:56 +0300

    FmService: Add FGS type
    E AndroidRuntime: FATAL EXCEPTION: main
    E AndroidRuntime: Process: com.android.fmradio, PID: 4295
    E AndroidRuntime: java.lang.RuntimeException: Unable to stop activity {com.android.fmradio/com.android.fmradio.FmMainActivity}: android.app.MissingForegroundServiceTypeException: Starting FGS without a type  callerApp=ProcessRecord{37b5d49 4295:com.android.fmradio/1000} targetSDK=24
    E AndroidRuntime: 	at android.app.ActivityThread.callActivityOnStop(ActivityThread.java:6029)
    E AndroidRuntime: 	at android.app.ActivityThread.performStopActivityInner(ActivityThread.java:6001)
    E AndroidRuntime: 	at android.app.ActivityThread.handleStopActivity(ActivityThread.java:6065)
    E AndroidRuntime: 	at android.app.servertransaction.StopActivityItem.execute(StopActivityItem.java:43)
    E AndroidRuntime: 	at android.app.servertransaction.ActivityTransactionItem.execute(ActivityTransactionItem.java:63)
    E AndroidRuntime: 	at android.app.servertransaction.TransactionExecutor.executeLifecycleItem(TransactionExecutor.java:169)
    E AndroidRuntime: 	at android.app.servertransaction.TransactionExecutor.executeTransactionItems(TransactionExecutor.java:101)
    E AndroidRuntime: 	at android.app.servertransaction.TransactionExecutor.execute(TransactionExecutor.java:80)
    E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2823)
    E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:110)
    E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:248)
    E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:338)
    E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:9063)
    E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:596)
    E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:932)
    E AndroidRuntime: Caused by: android.app.MissingForegroundServiceTypeException: Starting FGS without a type  callerApp=ProcessRecord{37b5d49 4295:com.android.fmradio/1000} targetSDK=24
    E AndroidRuntime: 	at android.app.MissingForegroundServiceTypeException$1.createFromParcel(MissingForegroundServiceTypeException.java:53)
    E AndroidRuntime: 	at android.app.MissingForegroundServiceTypeException$1.createFromParcel(MissingForegroundServiceTypeException.java:49)
    E AndroidRuntime: 	at android.os.Parcel.readParcelableInternal(Parcel.java:5219)
    E AndroidRuntime: 	at android.os.Parcel.readParcelable(Parcel.java:5201)
    E AndroidRuntime: 	at android.os.Parcel.createExceptionOrNull(Parcel.java:3339)
    E AndroidRuntime: 	at android.os.Parcel.createException(Parcel.java:3328)
    E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:3311)
    E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:3253)
    E AndroidRuntime: 	at android.app.IActivityManager$Stub$Proxy.setServiceForeground(IActivityManager.java:7308)
    E AndroidRuntime: 	at android.app.Service.startForeground(Service.java:776)
    E AndroidRuntime: 	at com.android.fmradio.FmService.showPlayingNotification(FmService.java:2030)
    E AndroidRuntime: 	at com.android.fmradio.FmService.updatePlayingNotification(FmService.java:2113)
    E AndroidRuntime: 	at com.android.fmradio.FmMainActivity.onStop(FmMainActivity.java:685)
    E AndroidRuntime: 	at android.app.Instrumentation.callActivityOnStop(Instrumentation.java:1744)
    E AndroidRuntime: 	at android.app.Activity.performStop(Activity.java:9422)
    E AndroidRuntime: 	at android.app.ActivityThread.callActivityOnStop(ActivityThread.java:6021)
    E AndroidRuntime: 	... 15 more

--- src/com/android/fmradio/FmService.java
+++ src/com/android/fmradio/FmService.java
@@ -33,6 +33,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.ServiceInfo;
 import android.content.res.Configuration;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -2027,7 +2028,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
 
             Notification n = notificationBuilder.build();
             n.flags &= ~Notification.FLAG_NO_CLEAR;
-            startForeground(NOTIFICATION_ID, n);
+            startForeground(NOTIFICATION_ID, n, ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK);
         }
     }
 
@@ -2095,7 +2096,7 @@ public class FmService extends Service implements FmRecorder.OnRecorderStateChan
      * Show notification
      */
     public void showRecordingNotification(Notification notification) {
-        startForeground(NOTIFICATION_ID, notification);
+        startForeground(NOTIFICATION_ID, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK);
     }
 
     /**
